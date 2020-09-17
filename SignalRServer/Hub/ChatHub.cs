203ddd93dbe05c53b0b0303dd4f359c1f4cc79aa
Artur Sattarov <rehsatart@gmail.com>
2020-09-17 19:51:04 +0300

    server

---
+++ SignalRServer/Hub/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRServer.Hub
+{
+    public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+        private const string DefaultUser = "default";
+        private const string DefaultMessage = "hello";
+        
+        public async Task SendMessage(string user = DefaultMessage, string message = DefaultMessage)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
