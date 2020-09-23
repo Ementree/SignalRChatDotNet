41ab3e3fa8133a4881cac848add11c7cda427034
PrepConcede <44526727+PrepConcedeITIS@users.noreply.github.com>
2020-09-23 18:57:54 +0300

    WIP

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SignalRService} from './services/signal-r.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UserMessage} from './models/user-message';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+  constructor(private signalRService: SignalRService) {
+    // this.userMessages = signalRService.userMessages;
+  }
+
+  title = 'chat-client';
+  messageForm: FormGroup;
+
+  sendMessage(): void {
+    const userMessage: UserMessage = {
+      userName: this.messageForm.get('name').value,
+      text: this.messageForm.get('message').value
+    };
+    console.log(userMessage);
+    this.signalRService.sendMessage(userMessage);
+  }
+
+  ngOnInit(): void {
+    this.signalRService.startConnection();
+    this.messageForm = new FormGroup({
+      name: new FormControl('', [Validators.minLength(1), Validators.maxLength(20)]),
+      message: new FormControl('', [Validators.minLength(1), Validators.maxLength(100)])
+    });
+  }
 }
