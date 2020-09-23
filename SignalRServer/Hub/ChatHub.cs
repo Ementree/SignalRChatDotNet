f056a2266b16dfa66ba508f000db69881b97e402
Tagmir Gilyazov <gilyazov@hightech.group>
2020-09-23 22:12:37 +0300

    Method name fix

--- SignalRServer/Hub/ChatHub.cs
+++ SignalRServer/Hub/ChatHub.cs
@@ -2,15 +2,17 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.Extensions.Caching.Memory;
+using SignalRServer.Models;
 
 namespace SignalRServer.Hub
 {
     public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
     {
         // Тока не ржите...
+
         #region Authorization
-        private const string PasswordHash = "pswd";
-        private HashSet<string> _authorizations;
+        private static string _passwordHash = "pswd";
+        private static HashSet<string> _authorizations;
         #endregion
 
         public ChatHub()
@@ -21,7 +23,7 @@ namespace SignalRServer.Hub
         public async Task Authorize(string connectionId, string userName, string passwordHash)
         {
             string answer;
-            if (PasswordHash != passwordHash)
+            if (_passwordHash != passwordHash)
             {
                 answer = "Wrong password!";
 
@@ -36,15 +38,13 @@ namespace SignalRServer.Hub
             }
         }
 
-        public async Task SendMessage(string connectionId, string userName, string message)
+        public async Task SendMessage(UserMessage message)
         {
-            if (!_authorizations.Contains(connectionId))
-            {
-                await DenySendingMessage(userName);
-                return;
-            }
-
-            await Clients.All.SendAsync("SendMessage", userName, message);
+            if (message.Text.Length > 100 || message.Text.Length < 1 || 
+                message.UserName.Length > 20 || message.UserName.Length < 1)
+                await Clients.Caller.SendAsync("ReceiveMessage",
+                    new UserMessage {Text = "Чел ты", UserName = "System: "});
+            await Clients.All.SendAsync("ReceiveMessage", message);
         }
 
         public async Task DenySendingMessage(string userName)
